e2343bd68319fde5b86f2ae9a1fd9d141876ad7d
AmegaPN <95965729+AmegaPN@users.noreply.github.com>
2022-11-28 13:26:32 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -17,4 +17,22 @@ difference = 0;
 rightWristX = 0;
 leftWristX = 0;
 
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
 
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+    }
+}
+
+function draw()
+{
+    background('#6C91C2');
+    textSize(difference);
+    fill('#FFE787');
+    text('Amega', 50, 400);
+}
